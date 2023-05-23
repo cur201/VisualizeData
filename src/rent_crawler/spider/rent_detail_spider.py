524877cf08b55b97d9031898fa27bc7f5cdc6acf
Duong Trung Hieu <57822309+cur201@users.noreply.github.com>
2023-05-23 22:15:42 +0700

    update function in rent_detail_spider

--- src/rent_crawler/spider/rent_detail_spider.py
+++ src/rent_crawler/spider/rent_detail_spider.py
@@ -2,7 +2,7 @@
 import scrapy
 import os
 import pandas as pd
-from src import get_element_selector, get_element_str
+from src import get_element_selector, get_element_str, get_address, get_property_info, get_property_type
 
 
 class RentDetailSpider(scrapy.Spider):
@@ -35,9 +35,9 @@ class RentDetailSpider(scrapy.Spider):
         property_selector = get_element_selector(self._response, DIV_PROPERTY_SELECTOR)
 
         detail_item['price'] = self._get_rent_price(property_selector)
-        detail_item['addr'] = self._get_address(property_selector)
-        detail_item['room'] = self._get_property_info(property_selector)
-        detail_item['type'] = self._get_property_type(property_selector)
+        detail_item['addr'] = get_address(property_selector)
+        detail_item['room'] = get_property_info(property_selector)
+        detail_item['type'] = get_property_type(property_selector)
 
         self._export_to_csv(detail_item)
 
@@ -70,53 +70,3 @@ class RentDetailSpider(scrapy.Spider):
             rent_price = '-'
         print(f"Rent price: {rent_price}")
         return rent_price
-
-    def _get_address(self, property_selector):
-        address = ''
-        DIV_ADDRESS_SELECTOR = 'div[data-testid="listing-details__button-copy-wrapper"]'
-        TEXT_ADDRESS = 'h1::text'
-        div_selector = get_element_selector(property_selector, DIV_ADDRESS_SELECTOR)
-        if len(div_selector) >= 1:
-            address = get_element_str(div_selector, TEXT_ADDRESS)
-        else:
-            address = '-'
-        print(f"Address: {address}")
-        return address
-
-    def _get_property_info(self, property_selector):
-        property_info = ''
-        property_numbers_str = []
-        property_type_str = []
-
-        DIV_PROPERTY_INFO_SUMMARY = 'div[data-testid="property-features"]'
-        DIV_PROPERTY_INFO_SELECTOR = 'span[data-testid="property-features-feature"] > span'
-        SPAN_PROPERTY_INFO_TYPE_SELECTOR = 'span[data-testid="property-features-text"]'
-        property_div_selector = get_element_selector(property_selector, DIV_PROPERTY_INFO_SUMMARY)
-
-        property_div_selector = get_element_selector(property_div_selector, DIV_PROPERTY_INFO_SELECTOR)
-        for selector in property_div_selector:
-            property_numbers_str.append(get_element_str(selector, '::text'))
-            property_type = get_element_selector(selector, SPAN_PROPERTY_INFO_TYPE_SELECTOR)
-            property_type_str.append(get_element_str(property_type, '::text'))
-
-        if len(property_numbers_str) >= 1:
-            property_info = ', '.join(
-                [f"{number} {ptype}" for number, ptype in zip(property_numbers_str, property_type_str)])
-        else:
-            property_info = '-'
-        print(f"Property Info: {property_info}")
-        return property_info
-
-    def _get_property_type(self, property_selector):
-
-        property_type = ''
-
-        DIV_PROPERTY_TYPE = 'div[data-testid="listing-summary-property-type"] > span'
-        property_type_selector = get_element_selector(property_selector, DIV_PROPERTY_TYPE)
-
-        if len(property_type_selector) >= 1:
-            property_type = get_element_str(property_type_selector[0], "::text")
-        else:
-            property_type = ''
-        print(f"Property Type: {property_type}")
-        return property_type
