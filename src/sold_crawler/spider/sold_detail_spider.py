e802463aa6e5a062f4e80753a3827f29d562cc54
Duong Trung Hieu <57822309+cur201@users.noreply.github.com>
2023-05-23 23:53:39 +0700

    update sold export data

--- src/sold_crawler/spider/sold_detail_spider.py
+++ src/sold_crawler/spider/sold_detail_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+import csv
+import os
+import pandas as pd
 from src import get_element_selector, get_element_str, get_address, get_property_info, get_property_type, get_date_sold
 
 
@@ -30,6 +33,28 @@ class SoldDetailSpider(scrapy.Spider):
         detail_item['rooms'] = get_property_info(property_selector)
         detail_item['type'] = get_property_type(property_selector)
         detail_item['date_sold'] = get_date_sold(property_selector)
+
+        self._export_to_csv(detail_item)
+
+    def _export_to_csv(self, detail_item):
+        export_file = 'sold-data.csv'
+        output_dir = './res/data/'
+        output_path = os.path.join(output_dir, export_file)
+
+        if not os.path.exists(output_path):
+            with open(output_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['price', 'address', 'room info', 'type', 'sold date'])
+
+        data = {
+            'price': [detail_item['price']],
+            'addr': [detail_item['addr']],
+            'rooms': [detail_item['rooms']],
+            'type': [detail_item['type']],
+            'date_sold': [detail_item['date_sold']]
+        }
+        df = pd.DataFrame(data)
+        df.to_csv(output_path, mode='a', header=False, index=False)
 
     def _get_sold_price(self, property_selector):
         sold_price = ''
